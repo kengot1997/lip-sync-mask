379778292b12db360f3d5f43a7b955c3dfaae11d
kengot1997 <kengodivgrad@gmail.com>
2020-10-21 01:13:55 +0900

    commit

--- js/vtuber.js
+++ js/vtuber.js
@@ -75,7 +75,7 @@ var setupMicrophone = function(){
 navigator.getUserMedia(
 	{audio : true},
 	function(stream){
-		document.querySelector('audio').src = URL.createObjectURL(stream);
+		document.querySelector('audio').src = stream;
 		var audioContext = new AudioContext();
 		var analyser = audioContext.createAnalyser();
 		var timeDomain = new Float32Array(analyser.frequencyBinCount);
